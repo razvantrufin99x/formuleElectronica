01fb04b0b23fda4992a4f231ef537af7628291b9
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 22:17:03 +0200

    Add project files.

---
+++ formuleElectronica/OhmLawDC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace formuleElectronica
+{
+   public class OhmLawDC
+    {
+        public float I(float U, float R)
+        {
+            return U / R;
+        }
+        public float U(float I, float R)
+        {
+            return I * R;
+        }
+        public float R(float U, float I)
+        {
+            return U / I;
+        }
+        public float P(float U, float I)
+        {
+            return U * I;
+        }
+        public float W(float P, float t)
+        {
+            return P * t;
+        }
+
+    }
+}
